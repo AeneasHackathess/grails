2164fab98819e2cd7bab58bd7cf281ad1bf80e14
plutoattack <plutoattackme@gmail.com>
2014-11-29 14:48:44 +0200

    Distress feature from arduino added

--- grails-app/controllers/gr/meerkat/ArduinoController.groovy
+++ grails-app/controllers/gr/meerkat/ArduinoController.groovy
@@ -1,32 +1,43 @@
 package gr.meerkat
 
+import grails.converters.JSON
+
 class ArduinoController {
 
     def index() {
         def user=User.findByMac(params.mac)
         if(user){
+            if(params.int("panic")==1){
+                user.patient.panic=true
+            }
             Place place2=new Place(lat: params.lat,lng: params.lng)
             try {
                 place2.save(flush: true,failOnError: true)
-                def movement2=0
-                if(params.move==1){
-                    movement2=1
+                def movement2=false
+                if(params.int("move")==1){
+                    movement2=true
                 }
                 Measure measure=new Measure(timestamp: new Date(), pulseRate: params.pulse,movement: movement2,place: place2)
                 try {
                     measure.save(flush: true,failOnError: true)
                     user.patient.measures.add(measure)
+                    def res = [response: "ok"]
+                    render res as JSON
                 }catch (Exception e2){
                     println('error on measure save')
                     measure.errors.allErrors.each {
                         println it
                     }
+                    def res = [response: "failMeasure"]
+                    render res as JSON
                 }
             }catch (Exception e){
                 println('error on place save')
                 place2.errors.allErrors.each {
                     println it
                 }
+                def res = [response: "failPlace"]
+                render res as JSON
             }
         }
     }
